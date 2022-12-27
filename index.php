887f0884abdf9f9098215fdbc6deee70393e5db0
Jessica Daniel Joni <jessicadj112@gmail.com>
2022-12-27 17:38:13 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+$nama = "Jessica";
+
+$umur = 21;
+
+echo "nama saya <strong>$nama</strong>,  saya berusia $umur tahun.<br>";
+
+
+$namaAdik = "David";
+$umurAdik = 18;
+
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia$umurAdik tahun.<br>";
+
+
+
+$selisihumur = $umur - $umurAdik;
+
+echo "selisih umur saya dan kakak saya adalah $selisihumur tahun."
+
+?>
